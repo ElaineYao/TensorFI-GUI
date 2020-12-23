dcfd3e0dd4a1df18cd322cfaedba332052523f21
ElaineYao <elainedv111@gmail.com>
2020-12-23 20:59:25 +0800

    Add conditionally appearance of widgets.

--- TensorFI-GUI.py
+++ TensorFI-GUI.py
@@ -63,6 +63,25 @@ def addIns():
     insLabel1.grid(row=6, column=25)
     insLabel1.after(1000, insLabel1.destroy)
 
+# ------------------------
+# Callback function
+
+def on_trace_choice(name, index, mode):
+    refresh()
+
+def refresh( ):
+    choice = injectCombo.get()
+    if choice == 'errorRate':
+        opsCombo.configure(state="normal")
+        opsEntry.config(state='normal')
+        insCombo.configure(state="disabled")
+        insEntry.config(state='disabled')
+    else:
+        opsCombo.configure(state="disabled")
+        opsEntry.config(state='disabled')
+        insCombo.config(state='normal')
+        insEntry.config(state='normal')
+
 #--------------
 # Parameters Part
 #Labels
@@ -90,9 +109,11 @@ skipLabel = tk.Label(top, font = ("Times New Roman", 10), text="SkipCount: ")\
                     .grid(row = 9, column = 10, padx = 10, pady = 25)
 
 # Combobox
-injectCombo = ttk.Combobox(top, font = ("Times New Roman", 10), values=['errorRate', 'dynamicInstance', 'oneFaultPerRun'])
+choiceVar = tk.StringVar()
+injectCombo = ttk.Combobox(top, font = ("Times New Roman", 10), textvariable=choiceVar, values=['errorRate', 'dynamicInstance', 'oneFaultPerRun'])
 injectCombo.grid(row=5, column=5)
 injectCombo.current(0)
+choiceVar.trace("w", on_trace_choice)
 
 scalarCombo = ttk.Combobox(top, font = ("Times New Roman", 10), values=['None', 'Rand', 'Zero', 'Rand-element', 'bitFlip-element', 'bitFlip-tensor'])
 scalarCombo.grid(row=6, column=5)
@@ -112,7 +133,7 @@ opsCombo.current(0)
 insCombo = ttk.Combobox(top, font = ("Times New Roman", 10), values=['ABSOLUTE', 'ADD', 'ASSIGN', 'ALL', 'ARGMAX', 'ARGMIN', 'BIASADD', 'CAST', 'COUNT-NONZERO', 'CONV2D', 'ELU', 'END', 'EQUAL', 'EXPAND-DIMS',
                                                                         'FILL', 'FLOOR-MOD', 'GREATER-EQUAL', 'IDENTITY', 'LESS-EQUAL', 'LOG', 'LRN', 'MATMUL', 'MAX-POOL', 'MEAN', 'MINIMUM', 'MUL', 'NEGATIVE', 'NOT-EQUAL', 'NOOP',
                                                                         'ONE-HOT', 'PACK', 'POW', 'RANDOM_UNIFORM', 'RANK', 'RANGE', 'REALDIV', 'RELU', 'RESHAPE', 'RSQRT', 'SIGMOID', 'SIZE', 'SHAPE', 'SOFT-MAX', 'SQUARE', 'STRIDED-SLICE',
-                                                                        'SUB',  'SUM', 'SWITCH', 'TANH', 'UNPACK'])
+                                                                        'SUB',  'SUM', 'SWITCH', 'TANH', 'UNPACK'], state="disabled")
 insCombo.grid(row=8, column=5)
 insCombo.current(0)
 
@@ -121,7 +142,7 @@ insCombo.current(0)
 opsEntry = tk.Entry(top, bd=5)
 opsEntry.grid(row=7, column = 15)
 opsEntry.focus()
-insEntry = tk.Entry(top, bd=5)
+insEntry = tk.Entry(top, bd=5, state="disabled")
 insEntry.grid(row=8, column = 15)
 insEntry.focus()
 seedText = tk.StringVar()
@@ -161,10 +182,12 @@ def browseConfFiles():
                                                   filetypes=(("YAML files",
                                                               "*.yaml*"),))
     if filename != '':
-        fileConfButt.configure(text=filename)
-#TODO: Add browse logDir
-def browseLogDir():
+        logdButt.configure(text=filename)
 
+def browseLogDir():
+    dirname = tkinter.filedialog.askdirectory()
+    if dirname != '':
+        logdButt.configure(text=dirname)
 
 #TODO: Add exceptions
 def injectFaults():
@@ -172,9 +195,30 @@ def injectFaults():
     parse_src_import = inCd.addImport(filename)
     loglValue = eval(loglCombo.get())
 
-    parse_src_fi = inCd.addFi(parse_src_import, fileConfButt.cget('text'), logdEntry.get(), loglValue, disCombo.get(), nameEntry.get(), fipEntry.get())
+    parse_src_fi = inCd.addFi(parse_src_import, fileConfButt.cget('text'), logdButt.cget('text'), loglValue, disCombo.get(), nameEntry.get(), fipEntry.get())
     # Execute the parsed code
     exec(compile(parse_src_fi, filename="<ast>", mode="exec"))
+
+#-------
+# Callback function
+def on_trace_mode(name, index, mode):
+    refresh_mode()
+def refresh_mode( ):
+    mode = modeCombo.get()
+    if mode == 'Run':
+        loglLabel.grid_forget()
+        loglCombo.grid_forget()
+        namelabel.grid_forget()
+        nameEntry.grid_forget()
+        fiplabel.grid_forget()
+        fipEntry.grid_forget()
+    else:
+        loglLabel.grid(row=13, column=10)
+        loglCombo.grid(row=13, column=15)
+        namelabel.grid(row=14, column=0)
+        nameEntry.grid(row=14, column=5)
+        fiplabel.grid(row=14, column=10)
+        fipEntry.grid(row=14, column=15)
 
 
 # Label
@@ -183,25 +227,37 @@ sourLabel = tk.Label(top, text="Source file: ", font = ("Times New Roman", 10)).
 confLabel = tk.Label(top, text="configFileName:", font = ("Times New Roman", 10)).grid(row=11, column=10, padx = 10, pady = 25)
 logdLabel = tk.Label(top, text="logDir:", font = ("Times New Roman", 10)).grid(row=12, column=0, padx = 10, pady = 25)
 disLabel = tk.Label(top, text="disableInjections:", font = ("Times New Roman", 10)).grid(row=12, column=10, padx = 10, pady = 25)
+modeLabel = tk.Label(top, text="Mode:", font = ("Times New Roman", 10)).grid(row=13, column=0, padx = 10, pady = 25)
 # Debugging mode
-loglLabel = tk.Label(top, text="logLevel:", font = ("Times New Roman", 10)).grid(row=13, column=00, padx = 10, pady = 25)
-namelabel = tk.Label(top, text="name:", font = ("Times New Roman", 10)).grid(row=13, column=10, padx = 10, pady = 25)
-fiplabel = tk.Label(top, text="fiPrefix:", font = ("Times New Roman", 10)).grid(row=14, column=0, padx = 10, pady = 25)
+loglLabel = tk.Label(top, text="logLevel:", font = ("Times New Roman", 10))
+# loglLabel.grid(row=13, column=10, padx = 10, pady = 25)
+namelabel = tk.Label(top, text="name:", font = ("Times New Roman", 10))
+# namelabel.grid(row=14, column=0, padx = 10, pady = 25)
+fiplabel = tk.Label(top, text="fiPrefix:", font = ("Times New Roman", 10))
+# fiplabel.grid(row=14, column=10, padx = 10, pady = 25)
 
 # Combobox
 disCombo = ttk.Combobox(top, font = ("Times New Roman", 10), values=['False', 'True'])
 disCombo.grid(row=12, column=15)
 disCombo.current(0)
+
+modeVar = tk.StringVar()
+modeCombo = ttk.Combobox(top, font = ("Times New Roman", 10), textvariable=modeVar, values=['Run', 'Debug'])
+modeCombo.grid(row=13, column=5)
+modeCombo.current(0)
+modeVar.trace("w", on_trace_mode)
+
+# Default to be 0
 loglCombo = ttk.Combobox(top, font = ("Times New Roman", 10), values=[10, 20, 30])
-loglCombo.grid(row=13, column=5)
+# loglCombo.grid(row=13, column=15)
 loglCombo.current(0)
 
 # Entry
 nameEntry = tk.Entry(top,bd=3)
-nameEntry.grid(row=13, column=15)
+# nameEntry.grid(row=14, column=5)
 nameEntry.focus()
 fipEntry = tk.Entry(top,bd=3)
-fipEntry.grid(row=14, column=5)
+# fipEntry.grid(row=14, column=15)
 fipEntry.focus()
 
 # Button
@@ -210,9 +266,9 @@ fileButt.grid(row=11, column=5)
 fileConfButt = tk.Button(top, font = ("Times New Roman", 10),text="Select YAML file", command=browseConfFiles)
 fileConfButt.grid(row=11, column=15)
 logdButt = tk.Button(top, font = ("Times New Roman", 10),text="Select log directory ", command=browseLogDir)
-logdButt.grid(row=12, column=15)
+logdButt.grid(row=12, column=5)
 fiButt = tk.Button(top, font = ("Times New Roman", 10),text="Inject faults", command=injectFaults)
-fiButt.grid(row=14, column=15)
+fiButt.grid(row=13, column=20)
 
 # -------------------------------
 # Statistics
