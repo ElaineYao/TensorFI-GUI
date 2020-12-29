24266a36fc9f27a29f0de7d1920979dee055891f
ElaineYao <elainedv111@gmail.com>
2020-12-29 11:51:29 +0800

    Integrate all code

--- insertCode.py
+++ insertCode.py
@@ -3,6 +3,7 @@ import astor
 import logging
 
 # ------------------------- Add `import TensorFI as ti` -----------------------------------
+# FIXME: Add import numpy as np
 def addImport(fPath):
 
     i = 0;
@@ -22,80 +23,16 @@ def addImport(fPath):
     # print(astor.to_source(parse_src))
     return parse_src
 
-# ------------------------------- Add TensorFI init function -----------------------------------
-# `fi = ti.TensorFI(sess,configFileName='./confFiles/eb/default-1eb.yaml', logLevel = 10, name = "lenet", disableInjections=False)`
-
-def addFi(parse_src, # Parsed code
-          configFileName="confFiles/default.yaml",  # Config file for reading fault configuration
-          logDir="faultLogs/",  # Log directory for the Fault log (Not to be confused with the logging level below)
-          logLevel=logging.DEBUG,  # Logging level {DEBUG=10, INFO=20, ERROR=30}
-          disableInjections=False,  # Should we disable injections after instrumenting ?
-          name="NoName",  # The name of the injector, used in statistics and logging
-          fiPrefix="fi_"):  # Prefix to attach to each node inserted for fault injection
-
-    # org_parse = parse_src
-    # print(ast.dump(ast.parse(org_parse)))
-
-    s = 'tf.Session' # This is the session from tensorFlow
-    i=0 # Index in body node
-    j=0 # Index in with node
-
-    for node in ast.iter_child_nodes(parse_src):
-        i = i+1
-        # Find 'with tf.Session as sess', instead of other 'with' expressions
-        if isinstance(node, ast.With) and node.context_expr.func.attr == 'Session':
-            s = node.optional_vars.id
-            break
-
-    withNode = parse_src.body[i-1]
-
-    for node in ast.walk(withNode):
-        j = j+1
-
-    sessName = ast.Name(s, ast.Load())
-    sessNameList = []
-    sessNameList.append(sessName)
-    tiName = ast.Name('ti', ast.Load())
-    fiName = ast.Name('fi', ast.Store())
-    fiNameList = []
-    fiKeyList = []
-    fiNameList.append(fiName)
-    confKey = ast.keyword('configFileName', ast.Str(configFileName))
-    logDKey = ast.keyword('logDir', ast.Str(logDir))
-    logKey = ast.keyword('logLevel', ast.Num(logLevel))
-    disaKey = ast.keyword('disableInjections', ast.Name(disableInjections, ast.Load()))
-    nameKey = ast.keyword('name', ast.Str(name))
-    preKey = ast.keyword('fiPrefix', ast.Str(fiPrefix))
-    fiKeyList.append(confKey)
-    fiKeyList.append(logDKey)
-    fiKeyList.append(logKey)
-    fiKeyList.append(disaKey)
-    fiKeyList.append(nameKey)
-    fiKeyList.append(preKey)
-    tiAttr = ast.Attribute(tiName, 'TensorFI', ast.Load())
-    tiCall = ast.Call(tiAttr, sessNameList, fiKeyList, None, None)
-    tiBody = ast.Assign(fiNameList, tiCall)
-
-    withNode.body.insert(j, tiBody)
-    ast.fix_missing_locations(parse_src)
-
-    # print(ast.dump(ast.parse(org_parse)))
-    print(astor.to_source(parse_src))
-    return parse_src
-
-
 # ---------------------------- Add code to find the correctIndex -----------------------------------------
 # `correct = sess.run(correct_prediction, feed_dict={x: X_test, y: y_test})
 #  correctIndex = np.argwhere(correct == True)
 #  correctIndex = correctIndex.flatten()`
 # FIXME: Add exception to determine whether the function the User insert is correct
-# E.g., parse_src = addCorrect(ast.parse(source), 'correct_prediction', {'x': 'X_test', 'y': 'y_test'})
-def addCorrect(parse_src, corrFun, feed_dict):
-    # FIXME: Add location to insert
+# E.g., parse_src = addCorrect('correct_prediction', {'x': 'X_test', 'y': 'y_test'}, 'sess')
+def addCorrect(corrFun, feed_dict, sess):
     body, tar1, tar2, tar3 = [], [], [], []
 
-    # FIXME: Replace 'sess'; Add import numpy as np
-    func1 = ast.Attribute(ast.Name('sess', ast.Load()), 'run', ast.Load())
+    func1 = ast.Attribute(ast.Name(sess, ast.Load()), 'run', ast.Load())
     func2 = ast.Attribute(ast.Name('np', ast.Load()), 'argwhere', ast.Load())
     func3 = ast.Attribute(ast.Name('correctIndex', ast.Load()), 'flatten', ast.Load())
 
@@ -126,13 +63,11 @@ def addCorrect(parse_src, corrFun, feed_dict):
     tar2.append(ast.Name('correctIndex', ast.Store()))
     tar3.append(ast.Name('correctIndex', ast.Store()))
 
-    # FIXME: Add location to insert
-    parse_src = ast.Module(body)
-    return parse_src
+    return body
 
 
 
-# ---------------- FIXME: Add new reshape code -------------------------
+# ----------------  Add new reshape code -------------------------
 # `totalSDC = 0
 #  totalFI = 10
 #  resFile = open("lenet-bitFI10.csv", "a")
@@ -192,9 +127,7 @@ def addSDC(filename, totFI, Xtest, ytest):
     body.append(ass8)
     body.append(ass9)
 
-    # FIXME: Add location to insert
-    parse_src = ast.Module(body)
-    return parse_src
+    return body
 
 # ------------------- Add for loop to calculate SDC -------------------------
 # `    for i in range(10):
@@ -211,9 +144,9 @@ def addSDC(filename, totFI, Xtest, ytest):
 #     SDCrates = totalSDC/(10.0*totalFI)
 #     resFile.write(str(SDCrates))
 #     print("SDC rates: ", SDCrates)
-# E.g., parse_src = addLoop('X_test', 'y_test', {'x': 'X_test', 'y': 'y_test'})
+# E.g., parse_src = addLoop('X_test', 'y_test', {'x': 'X_test', 'y': 'y_test'}, 'sess')
 
-def addLoop(Xtest, ytest, feed_dict):
+def addLoop(Xtest, ytest, feed_dict, sess):
 
     body, fbody, fName1, fName2, fName3, fNum, aTarg, exargs, eCargs, pvals, pelts, arg3, arg4, argfor, bodyfor= [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
 
@@ -241,8 +174,7 @@ def addLoop(Xtest, ytest, feed_dict):
     ifbody.append(ast.AugAssign(ast.Name('SDC', ast.Store()), ast.Add(), ast.Num(1)))
 
     argfor.append(ast.Name('totalFI', ast.Load()))
-    # FIXME: Replace sess
-    bodyfor.append(ast.Assign(bodyTar, ast.Call(ast.Attribute(ast.Name('sess', ast.Load()), 'run', ast.Load()), bodyAttrArgs,
+    bodyfor.append(ast.Assign(bodyTar, ast.Call(ast.Attribute(ast.Name(sess, ast.Load()), 'run', ast.Load()), bodyAttrArgs,
                                        bodyKey, None, None)))
     bodyfor.append(ast.If(ast.Compare(ast.Name('acy', ast.Load()), ops, ifname), ifbody, []))
 
@@ -281,13 +213,96 @@ def addLoop(Xtest, ytest, feed_dict):
     body.append(expr)
     body.append(prit)
 
-    # FIXME: Add location to insert
-    parse_src = ast.Module(body)
+    return body
+
+
+# ------------------------------- Add TensorFI init function -----------------------------------
+# `fi = ti.TensorFI(sess,configFileName='./confFiles/eb/default-1eb.yaml', logLevel = 10, name = "lenet", disableInjections=False)`
+# E.g.,  parse_src_fi = addFi(parse_src, 'correct_prediction', {'x': 'X_test', 'y': 'y_test'}, 'lenet-sdcrates.csv', 10,'X_test', 'y_test',
+#                          'testGen.yaml', "faultLogs/", logging.DEBUG, 'False', 'convolutional', 'fi_')
+
+def addFi(parse_src, # Parsed code
+          corrFun, # Function to get the correct prediction
+          feed_dict, # feed_dict in sess.run()
+          filename, # Name of file to restore the SDC rates.
+          totFI, # Number of fault injection
+          Xtest, # Variable name of testset X
+          ytest, # Variable name of testset Y
+          configFileName="confFiles/default.yaml",  # Config file for reading fault configuration
+          logDir="faultLogs/",  # Log directory for the Fault log (Not to be confused with the logging level below)
+          logLevel=logging.DEBUG,  # Logging level {DEBUG=10, INFO=20, ERROR=30}
+          disableInjections=False,  # Should we disable injections after instrumenting ?
+          name="NoName",  # The name of the injector, used in statistics and logging
+          fiPrefix="fi_"):  # Prefix to attach to each node inserted for fault injection
+
+
+    s = 'tf.Session' # This is the session from tensorFlow
+    i=0 # Index in body node
+    j=0 # Index in with node
+
+    for node in ast.iter_child_nodes(parse_src):
+        i = i+1
+        # Find 'with tf.Session as sess', instead of other 'with' expressions
+        if isinstance(node, ast.With) and node.context_expr.func.attr == 'Session':
+            s = node.optional_vars.id
+            break
+
+    withNode = parse_src.body[i-1]
+
+    pos = len(withNode.body)
+
+    sessName = ast.Name(s, ast.Load())
+    sessNameList = []
+    sessNameList.append(sessName)
+    tiName = ast.Name('ti', ast.Load())
+    fiName = ast.Name('fi', ast.Store())
+    fiNameList = []
+    fiKeyList = []
+    fiNameList.append(fiName)
+    confKey = ast.keyword('configFileName', ast.Str(configFileName))
+    logDKey = ast.keyword('logDir', ast.Str(logDir))
+    logKey = ast.keyword('logLevel', ast.Num(logLevel))
+    disaKey = ast.keyword('disableInjections', ast.Name(disableInjections, ast.Load()))
+    nameKey = ast.keyword('name', ast.Str(name))
+    preKey = ast.keyword('fiPrefix', ast.Str(fiPrefix))
+    fiKeyList.append(confKey)
+    fiKeyList.append(logDKey)
+    fiKeyList.append(logKey)
+    fiKeyList.append(disaKey)
+    fiKeyList.append(nameKey)
+    fiKeyList.append(preKey)
+    tiAttr = ast.Attribute(tiName, 'TensorFI', ast.Load())
+    tiCall = ast.Call(tiAttr, sessNameList, fiKeyList, None, None)
+
+    tiBody = []
+    tiBody.append(ast.Assign(fiNameList, tiCall))
+
+    corrBody = addCorrect(corrFun, feed_dict, s)
+    for i in corrBody:
+        tiBody.append(i)
+
+    sdcBody = addSDC(filename, totFI, Xtest, ytest)
+    for i in sdcBody:
+        tiBody.append(i)
+
+    loopBody = addLoop(Xtest, ytest, feed_dict, s)
+    for i in loopBody:
+        tiBody.append(i)
+
+    for i in range(len(tiBody)):
+        withNode.body.insert(pos+i, tiBody[i])
+
+    ast.fix_missing_locations(parse_src)
+
+    print(astor.to_source(parse_src))
+    # print(ast.dump(parse_src))
     return parse_src
+
 
 if __name__ == '__main__':
     parse_src_import=addImport('lenet-mnist-no-FI.py')
-    parse_src_fi = addFi(parse_src_import,  'testGen.yaml', "faultLogs/", logging.DEBUG, 'False', 'convolutional', 'fi_')
+    parse_src_fi =addFi(parse_src_import, 'correct_prediction', {'x': 'X_test', 'y': 'y_test'}, 'lenet-sdcrates.csv', 10,'X_test', 'y_test',
+                         'testGen.yaml', "faultLogs/", logging.DEBUG, 'False', 'convolutional', 'fi_')
     # Execute the parsed code
     # parse_src_import = ast.parse(open('sample.py').read())
     # global_env = {}
